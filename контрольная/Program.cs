6eef122f54a1e3eeebfc985844540741cb96f5c3
«IlyaAntonov» <antonovilya82@gmail.com>
2022-12-25 22:34:49 +0500

    добавлена программа

--- контрольная/Program.cs
+++ контрольная/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа
 
-Console.WriteLine("Hello, World!");
+string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M )
+    {
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+    }
+Console.WriteLine("Массив строк, длина которых меньше или равна 3 символам: ");
+Console.Write(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
